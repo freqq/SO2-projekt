85c8d03562284e639e1965479f092cbab9c53dea
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-03-02 21:59:03 +0100

    Include needed modules

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <atomic>
+#include <ncurses.h>
+#include <mutex>
+#include <unistd.h>
+#include "Philosopher.hpp"
+#include "Fork.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
