d31a982ae0cb106a70e10d616c9ee69c7bf20d47
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-03-29 16:15:18 +0200

    Start over

--- main.cpp
+++ main.cpp
@@ -1,86 +1,10 @@
 #include <iostream>
-#include <thread>
-#include <vector>
-#include <atomic>
-#include <ncurses.h>
-#include <mutex>
-#include <unistd.h>
-#include "Philosopher.hpp"
-#include "Fork.hpp"
 
-
-std::vector<Philosopher> philosophers;
-const int THREADS_NUMBER = 5;
-
-void philisopher_lifecycle(Philosopher* philosopher, std::atomic<bool>& running) {
-    while(running) {
-        philosopher -> think();
-        philosopher -> eat();
-    }
-}
-
-void display(std::atomic<bool>& displaying) {
-    while(displaying) {
-        clear();
-        for(int i = 0; i < THREADS_NUMBER; i++) {
-            mvprintw(i+1, 0, philosophers[i].get_name().c_str());
-
-            switch (philosophers[i].get_state()){
-                case State::EATING:
-                    mvprintw(i+1, 15, "eating");
-                    break;
-                case State::THINKING:
-                    mvprintw(i+1, 15, "thinking");
-                    break;
-            }
-
-            mvprintw(i+1, 40, std::to_string(philosophers[i].get_progress()).c_str());
-        }
-
-        refresh();
-
-        usleep(10000);
-    }
-}
-
+using namespace std;
 
 int main() {
-    srand(time(NULL));
 
-    initscr();
-    refresh();
-
-    std::thread threads(THREADS_NUMBER);
-
-    for(int i = 0; i < THREADS_NUMBER; i++){
-        std::string name("philosopher");
-        philosophers.push_back(new Philosopher(new Fork(), new Fork(), name + std::to_string(i)));
-    }
-
-    std::atomic<bool> running(true);
-
-    for(int i = 0; i < THREADS_NUMBER; i++)
-        threads[i] = std::thread(philisopher_lifecycle, philosophers[i], std::ref(running));
-
-    std::atomic<bool> displaying(true);
-
-    std::thread displayThread(display, ref(displaying));
-
-    int esc = 0;
-    do {
-        noecho();
-        esc = getch();
-    } while (esc != 27);
-
-    running = false;
-    displaying = false;
-
-    for(int i = 0; i < THREADS_NUMBER; i++)
-        threads[i].join();
-
-    displayThread.join();
-
-    endwin();
+    cout << "hehe";
 
     return 0;
 }
